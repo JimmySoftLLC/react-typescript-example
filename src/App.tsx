474273426bcbae1793e88b88fc50c1d5e58df1d1
jbailey <jbailey@jimmysoftllc.com>
2021-05-25 16:00:35 -0400

    Added storybook to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+let myString: string = "Is typescript being used? ";
+let isTypeScriptBeingUsed: boolean = true;
+let sentence: string = myString + isTypeScriptBeingUsed;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {sentence}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
